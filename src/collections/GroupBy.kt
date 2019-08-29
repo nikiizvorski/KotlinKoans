03db7b0275787d3314144962603b1021f16008ea
nikiizvorski <nikiizvorski@gmail.com>
2019-08-29 16:31:52 +0300

    Customs in GroupBy

--- src/collections/GroupBy.kt
+++ src/collections/GroupBy.kt
@@ -7,6 +7,11 @@ fun Shop.groupCustomersByCity(): Map<City, List<Customer>> = customers.groupBy{
     it.city
 }
 
+// Return a map of the customers with highest orders number and sorted
+fun Shop.getCustomerByOrdersNumber(): Map<Int, List<Customer>> = customers.groupBy {
+    it.orders.size
+}.toSortedMap()
+
 /**
  *
  * https://play.kotlinlang.org/koans/Collections/GroupBy/Task.kt
@@ -16,4 +21,5 @@ fun Shop.groupCustomersByCity(): Map<City, List<Customer>> = customers.groupBy{
 
 fun main(args: Array<String>) {
     println(shop.groupCustomersByCity())
+    print(shop.getCustomerByOrdersNumber())
 }
