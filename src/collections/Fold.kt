22015988fffe30268aea236a9e5bfb86cf370113
nikiizvorski <nikiizvorski@gmail.com>
2019-09-03 09:06:34 +0300

    Added example reduce/fold in Kotlin

--- src/collections/Fold.kt
+++ src/collections/Fold.kt
@@ -12,6 +12,20 @@ fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
 }
 
 /**
+ * fold() takes an initial value, and the first invocation of the lambda you pass to it will receive that initial value
+ * and the first element of the collection as parameters.
+ *
+ * reduce() doesn't take an initial value, but instead starts with the first element of
+ * the collection as the accumulator (called sum in the following example).
+ */
+fun smallExamplesFold(){
+    val list = listOf(1, 2, 3).fold(0) { sum, element -> sum + element }
+    val plist = listOf(1, 2, 3).reduce {sum, element -> sum + element}
+    println(list)
+    println(plist)
+}
+
+/**
  *
  * https://play.kotlinlang.org/koans/Collections/Fold/Task.kt
  *
@@ -20,4 +34,5 @@ fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
 
 fun main(args: Array<String>) {
     println(shop.getSetOfProductsOrderedByEveryCustomer())
+    smallExamplesFold()
 }
